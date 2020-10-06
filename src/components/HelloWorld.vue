5aec1ceaf0673baa92ec6cc5622b6e68df99a3e2
jarobober <jarek.bober@gmail.com>
2020-10-06 19:56:18 +0200

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,149 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">
+                  Id
+                </th>
+                <th class="text-left">
+                  Name
+                </th>
+                <th class="text-left">
+                  Description
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="category in categories.edges"
+                :key="category.node.id"
+              >
+                <td class="text-left">
+                  {{ category.node.id }}
+                </td>
+                <td class="text-left">
+                  {{ category.node.name }}
+                </td>
+                <td class="text-left">
+                  {{ category.node.description }}
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+      <v-col cols="6" class="mt-5">
+        <v-form ref="form">
+          <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="Password"
+            required
+          ></v-text-field>
+          <div class="d-flex justify-space-between mt-4">
+            <v-btn @click="accountRegister()">Sign up</v-btn>
+            <v-btn @click="tokenCreate()">Log in</v-btn>
+          </div>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: function()  {
+    return {
+      email: '',
+      password: '',
+      categoriesNumber: 10
+    }
+  },
+
+  methods: {
+    accountRegister() {
+      const mail = this.email
+      const pass = this.password
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation {
+            accountRegister(input: {
+              email: $email
+              password: $password
+              redirectUrl: "https://localhost:3000"
+            }), {
+              accountErrors {
+                message
+              }
+              requiresConfirmation
+              user {
+                email
+                id
+              }
+            }
+          }
+        `,
+        variables: {
+          email: mail,
+          password: pass
+        }
+      })
+      alert('Account registered')
+    },
+    tokenCreate() {
+      const mail = this.email
+      const pass = this.password
+      this.$apollo.mutate({
+        mutation: gql`
+          mutation {
+            tokenCreate(email: $email, password: $password) {
+              token
+              user {
+                id
+                email
+              }
+              errors {
+                field
+                message
+              }
+            }
+          }
+        `,
+        variables: {
+          email: mail,
+          password: pass
+        }
+      })
+      alert('Token created')
+    }
+  },
+
+  apollo: {
+    categories: gql`
+      query {
+        categories(first: 5) {
+          edges {
+            node {
+              id
+              name
+              description
+            }
+          }
+        }
+      }
+    `
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
